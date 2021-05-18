3bcd9fff4ce4cc1f31dfe174a6cb09d578c3c07c
RikoBgrff <riko.bagirli777@gmail.com>
2021-05-18 13:31:04 +0400

    Add project files.

---
+++ PubApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubApp.Models
+{
+    public class Product : Entity
+    {
+        public string Product_Name { get; set; }
+        public string Product_Price { get; set; }
+        public string Product_Quantity { get; set; }
+        public string ImagePath { get; set; }
+        public string Details { get; set; }
+
+    }
+}
